4bf9b83bb51445708bafb57e9ef1abe444ba242a
Araz1998 <33088732+Araz1998@users.noreply.github.com>
2020-05-14 23:07:33 +0300

    working!!

---
+++ src/main/java/com/message/Repo/UserRepo.java
@@ -0,0 +1,8 @@
+package com.message.Repo;
+
+import com.message.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
---
+++ src/main/java/com/message/model/Role.java
@@ -0,0 +1,5 @@
+package com.message.model;
+
+public enum Role {
+    USER;
+}
